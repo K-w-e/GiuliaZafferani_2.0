7ba761901667229338e2920fa1ed8e494df38d7c
Alberto Guerra <a.alb.guerra@gmail.com>
2022-09-11 16:15:39 +0200

    Update style

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ function toggleDark() {
 
     if (document.body.style.backgroundColor == "black"){
         document.getElementById("mode").innerHTML = "-> Dark Mode"
-        document.getElementsByTagName("text")[0].style.fill = "red"
+        document.getElementsByTagName("text")[0].style.fill = "#853cf4"
         document.body.style.backgroundColor = "white"
     }
     else{
         document.getElementById("mode").innerHTML = "-> Light Mode"
-        document.getElementsByTagName("text")[0].style.fill = "green"
+        document.getElementsByTagName("text")[0].style.fill = "white"
         document.body.style.backgroundColor = "black"
     }
 }
